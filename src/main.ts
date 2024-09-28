7c7cf942aff934c71ce1dc56eca76261d61b02a6
Luis Mac <luismacod5@gmail.com>
2024-09-28 17:03:11 -0600

    Refactor main.ts to add Swagger documentation for the API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API RESTful para Users')
+    .setDescription('Reto 7: REST y HTTP para obtener y generar datos')
+    .setVersion('1.0')
+    .addTag('DigitalNAO')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
