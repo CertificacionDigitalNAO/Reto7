5f04b85c1cfe853425b7e8e2324c93c520b34d0c
Luis Mac <luismacod5@gmail.com>
2024-10-01 20:18:12 -0600

    Configura Swagger para servir archivos estáticos en Vercel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './docs/swagger.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as express from 'express';
 
 /**
  * Función principal para iniciar la aplicación NestJS.
@@ -10,10 +13,13 @@ import { setupSwagger } from './docs/swagger.config';
  * @returns {Promise<void>} Una promesa que se resuelve cuando la aplicación ha iniciado.
  */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Configura Swagger para la documentación de la API
   setupSwagger(app);
+
+  // Configura el servidor para servir archivos estáticos
+  app.use('/api/docs', express.static(join(__dirname, '..', 'node_modules', 'swagger-ui-dist')));
 
   // Inicia la aplicación en el puerto 3000
   await app.listen(3000);
